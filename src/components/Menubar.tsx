c3d5eba4b9186683b2c612baa6fd223557c1e02f
Ajinkya Palaskar <palaskarajinkya22@gmail.com>
2024-12-18 08:59:07 +0530

    feat: put social links in the left side of menubar

--- src/components/Menubar.tsx
+++ src/components/Menubar.tsx
@@ -7,12 +7,9 @@ import LiveDate from "@/components/LiveDate";
 import Wallpapers from "@/components/Wallpapers";
 
 import AppleLight from "@/icons/apple-light.svg";
-import Github from "@/icons/github.svg";
-import LinkedIn from "@/icons/linkedin.svg";
 import Options from "@/icons/menu-options.svg";
 import SearchIcon from "@/icons/search.svg";
 import Wifi from "@/icons/wifi.svg";
-import YouTube from "@/icons/youtube.svg";
 
 const Menubar = () => {
   return (
@@ -22,43 +19,45 @@ const Menubar = () => {
 
         <span className="text-regular font-bold text-white">Finder</span>
 
-        <span className="text-regular font-semibold text-white">About</span>
-        <span className="text-regular font-semibold text-white">Projects</span>
-        <span className="text-regular font-semibold text-white">
-          Experience
-        </span>
-        <span className="text-regular font-semibold text-white">Education</span>
-      </div>
-
-      <div className="gap-x-4.5 flex items-center">
-        {/* social icons */}
+        {/* social links */}
         <a
           href="https://github.com/Ajinkyap22"
           target="_blank"
           rel="noopener noreferrer"
-          className="cursor-pointer"
+          className="text-regular cursor-pointer font-semibold text-white"
         >
-          <Image src={Github} alt="Github" className="h-4.5 w-4.5" />
+          GitHub
         </a>
 
         <a
           href="https://www.linkedin.com/in/ajinkya-palaskar"
           target="_blank"
           rel="noopener noreferrer"
-          className="cursor-pointer"
+          className="text-regular cursor-pointer font-semibold text-white"
         >
-          <Image src={LinkedIn} alt="LinkedIn" className="h-4.5 w-4.5" />
+          LinkedIn
+        </a>
+
+        <a
+          href="https://ajinkyap.hashnode.dev"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-regular cursor-pointer font-semibold text-white"
+        >
+          Blog
         </a>
 
         <a
           href="https://www.youtube.com/@victor_productions"
           target="_blank"
           rel="noopener noreferrer"
-          className="cursor-pointer"
+          className="text-regular cursor-pointer font-semibold text-white"
         >
-          <Image src={YouTube} alt="YouTube" className="h-4.5 w-4.5" />
+          YouTube
         </a>
+      </div>
 
+      <div className="gap-x-4.5 flex items-center">
         {/* wifi */}
         <Image src={Wifi} alt="Wifi" className="h-4.5 w-4.5" />
 
